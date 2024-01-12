8221d3c9dbaa197e030b25bd3aae397bfc6764e0
khoidang2110 <khoidang2110@gmail.com>
2024-01-12 09:39:25 +0700

    postman

--- src/controllers/foodControllers.js
+++ src/controllers/foodControllers.js
@@ -8,6 +8,25 @@ const getFood = async (req, res) => {
       let data = await conn.food.findAll(
 
       );
+
+
+      //exam join 3 table
+      // let data = await conn.restaurant.findAll({
+      //   include: [
+      //     {
+      //       model: conn.like_res,
+      //       as: "like_res",
+      //       include: [
+      //         {
+      //           model: conn.users,
+      //           as: "user"
+      //         }
+      //       ]
+      //     }
+      //   ]
+      // }
+        
+      // );
       res.send(data);
     } catch (error) {
       res.send(`BE error ${error}`);
